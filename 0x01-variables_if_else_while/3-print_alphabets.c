46dc2b44ea43f056a4267ada58dab66c13217d54
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2023-03-17 04:24:38 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * prints the alphabet in lowercase, followed by a new line.
+ * prints the alphabet in lowercase, and then in uppercase, followed by a new line.
  * Return: Always 0 (Success)
  */
 
