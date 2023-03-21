1c7a4fed56ab08ddace1e2d80aa19d86d8d4343b
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2023-03-21 03:22:56 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,13 +14,13 @@ void times_table(void)
 	{
 		for (j = 0; j < 9; j++)
 		{
-			k = i*j
+			k = i*j;
 			_putchar(k + '0');
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
 		}
-		k = i*j
+		k = i*j;
 		_putchar(k + '0');
 		_putchar('\n');
 	}
