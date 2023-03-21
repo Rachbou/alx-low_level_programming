2ce2e49009dbc9a712735174bdcef3e0fee9cbca
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2023-03-21 09:11:22 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * add - Entry point
+  * adds two integers and returns the result.
+  * @n: The first number.
+  * Return: ...
+  */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n, i < 98, i++)
+	{
+		printf('%d, ', i)
+	}
+	for (i = n, i > 98, i--)
+	{
+		printf('%d, ', i)
+	}
+	printf('%d\n', i)
+}
