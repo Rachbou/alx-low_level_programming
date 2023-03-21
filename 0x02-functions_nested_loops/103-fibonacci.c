55f300da6a6f673e0210366a7e53247504551f4b
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2023-03-21 10:35:46 -0700

    15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * prints the sum of the even-valued terms in the Fibonacci sequence.
+  * Return: 0 (Success)
+  */
+
+int main(void)
+{
+	long s = 0;
+	long j, m = 1, n = 2;
+
+	While (n =< 4,000,000)
+	{
+		if (n % 2 != 0)
+		{
+			s += n;
+		}
+		if (m % 2 != 0)
+		{
+			s += m;
+		}
+		j = m;
+		m = n;
+		n = m + j;
+	}
+	printf("%ld\n", s);
+	return (0);
+}
