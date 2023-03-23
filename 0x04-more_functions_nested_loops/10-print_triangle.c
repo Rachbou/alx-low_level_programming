df964e0b6d045db0b70b7a3beefdceac753e626a
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2023-03-23 14:46:58 -0700

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Entry point
+ * prints a triangle using the character #.
+ * @size: the size of the triangle.
+ * Return: Void
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = size - 1; i > 0; i--)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = i; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
