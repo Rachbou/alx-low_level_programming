0f85900ee964dc94fbe49108aa1fcfcc1f348f09
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2023-03-23 14:29:49 -0700

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints numbers from 1 to 100.
+ * prints Fizz instead of multiples of three.
+ * prints Buzz instead of multiples of five.
+ * prints FizzBuzz instead of multiples of three and five.
+ * @size: the size of the square.
+ * Return: Void
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf"%d", i);
+		}
+		if (i == 100)
+		{
+			continue
+		}
+		printf(" ");
+	}
+}
