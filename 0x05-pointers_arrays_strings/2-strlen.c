7aaba38acd758964b7e8648ceba654c00d7ba68e
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2023-03-27 12:53:14 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * returns the length of a string.
+ * @s: the string.
+ * Return: length of a string.
+ */
+ 
+int _strlen(char *s)
+{
+	int l = 0;
+	int i;
+
+	for(i=0; *s[i] != '\0'; i++)
+	{
+		l++;
+	}
+
+	return (l)
+}
