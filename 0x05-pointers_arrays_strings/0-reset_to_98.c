cb90d715eeafa216aab0e150689d33dced02adca
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2023-03-27 12:34:26 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * updates the value of an int parameter to to 98.
+ * @n: the integer to print.
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
