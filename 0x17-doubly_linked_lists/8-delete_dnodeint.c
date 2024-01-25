d78d0814b4df0147796fbd1ef9507932dedd498b
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-01-25 11:01:29 -0800

    8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -22,7 +22,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = (*head)->next;
 		if (*head != NULL)
 			(*head)->prev = NULL;
-		return (1)
+		return (1);
 	}
 	while (index > 0)
 	{
