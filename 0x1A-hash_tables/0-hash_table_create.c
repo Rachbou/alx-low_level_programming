333c582417f4bf873fcfa2f5ee3d1614704b950d
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-02-15 09:25:15 -0800

    0x1A. C - Hash tables : mandatory

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,8 +9,8 @@
  * hash_table_create - Creates a hash table.
  * @size: The size of the array.
  *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new hash table.
+ * Return: NULL if an error occurs.
+ *         Pointer to the new hash table otherwise.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
