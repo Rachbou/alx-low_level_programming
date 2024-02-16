8c3c64ee911e10aa10bb038e4b28deaaa2328fda
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-02-16 11:41:04 -0800

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL or array == NULL)
+	if (hash_table == NULL || array == NULL)
 	{
 		return (NULL);
 	}
