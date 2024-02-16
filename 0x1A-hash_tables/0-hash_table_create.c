26914c8762a0886d1b298ecfe5bec99c33bf93fb
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-02-16 11:37:28 -0800

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,20 +14,16 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
-	hash_node_t **array;
+	hash_table_t *hash_table  = malloc(sizeof(hash_table_t));
+	hash_node_t **array = malloc(sizeof(hash_node_t *) * size);
 	unsigned long int i;
 
 	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
+	if (hash_table == NULL or array == NULL)
 	{
 		return (NULL);
 	}
-	array = malloc(sizeof(hash_node_t *) * size);
-	if (array == NULL)
-	{
-		return (NULL);
-	}
+
 	for (i = 0; i < size; i++)
 	{
 		array[i] = NULL;
