32975458fbbb0390c3f2f4e6596babb271d264c5
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-02-16 11:55:17 -0800

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -30,7 +30,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	for (i = 0; i < size; i++)
 	{
-		hash_table->array[i] = NULL;
+		array[i] = NULL;
 	}
 	hash_table->size = size;
 	hash_table->array = array;
